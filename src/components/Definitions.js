d29a0cb2fd5b310fa8d50dad07963366b721ed53
Bharath Balasubramaniam <balasubramaniambharath7@gmail.com>
2022-02-04 21:31:51 +0530

    modified

--- src/components/Definitions.js
+++ src/components/Definitions.js
@@ -7,7 +7,7 @@ const Definitions = ({ word, category, meanings, lightTheme }) => {
       {meanings[0] && word && category === "en" && (
         <audio
           style={{ backgroundColor: "#fff", borderRadius: 10, width: "100%" }}
-          src={meanings[0].phonetics[0].audio}
+          src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
           controls
         >
           Your browser does not support the audio element.
